f856fd7e3737b364e4dd527029c24207c2202393
Rustemi Argjend <a01651643@unet.univie.ac.at>
2024-03-20 20:51:47 +0100

    implemented initial core controller methods

--- custom_ui/alpha_graph_ui/alpha_graph_controller.py
+++ custom_ui/alpha_graph_ui/alpha_graph_controller.py
@@ -1,3 +1,6 @@
+from api.pickle_save import pickle_load
+from mining_algorithms.alpha_mining import AlphaMining
+
 
 class AlphaGraphController:
     def __init__(self, workingDirectory):
@@ -5,3 +8,18 @@ class AlphaGraphController:
         self.model = None
         self.workingDirectory = workingDirectory
 
+    def start_mining(self, cases):
+        self.model = AlphaMining(cases)
+        self.mine_and_draw()
+        pass
+
+    def load_model(self, file_path):
+        self.model = pickle_load(file_path)
+        self.mine_and_draw()
+        return file_path
+
+    def mine_and_draw(self):
+        pass
+
+    def get_model(self):
+        return self.model
